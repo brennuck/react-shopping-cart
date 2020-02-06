471e7681827ebf5930ef3426aab7fce513f34ab9
Brennon Nuckols <brennuck@gmail.com>
2020-02-06 14:01:17 -0700

    Providing data with CartContext

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import data from './data';
 
 // Contexts
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -22,13 +23,12 @@ function App() {
 		<div className="App">
 			<Navigation cart={cart} />
 			<ProductContext.Provider value={{ products, addItem }}>
-				{/* Routes */}
-				<Route exact path="/" component={Products} />
+				<CartContext.Provider value={cart}>
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
+					<Route path="/cart" component={ShoppingCart} />
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
